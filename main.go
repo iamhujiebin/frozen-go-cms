db4bbe1b8b64d16d0dde3d217f84af5f24def6ae
JiebinHu <458249864@qq.com>
2023-05-04 21:53:38 +0800

    feat:静态服务器

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"fmt"
 	"frozen-go-cms/route"
+	"net/http"
 )
 
 const (
-	PORT = 8086
+	PORT        = 8086
+	STATIC_PORT = 8084
 )
 
 func main() {
-	// 静态文件服务器
-
+	go static()                     // 静态服务器
 	r := route.InitRouter()         // 注册路由
 	r.Run(fmt.Sprintf(":%d", PORT)) // 启动服务
+}
+
+func static() {
+	fs := http.FileServer(http.Dir("build/"))
+	http.Handle("/", fs)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", STATIC_PORT), nil)
 }
